4bdcf6c27ffd2462a35565dd519a2ab21ebfa66b
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-02-14 09:10:04 -0300

    main view design

--- src/main/java/com/example/javafxjdbcproject/ProjectApplication.java
+++ src/main/java/com/example/javafxjdbcproject/ProjectApplication.java
@@ -3,6 +3,7 @@ package com.example.javafxjdbcproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,13 @@ public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
+        stage.setTitle("JavaFx  Application");
         stage.setScene(scene);
         stage.show();
     }
